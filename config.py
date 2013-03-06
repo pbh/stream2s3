ec18ba69357bea253618770b9a76c18779769541
Artem Dudarev <dudarev@gmail.com>
2013-03-06 17:44:37 +0200

    8: initial config settings

---
+++ config.py
@@ -0,0 +1,9 @@
+# streamripper options
+
+RECORDING_INTERVAL_SECONDS = 10
+
+DESTINATION_DIRECTORY = '/tmp/stream2s3/'
+
+# required streamripper options are created from variables above
+# more options may be specified in this variable directly
+STREAMRIPPER_OPTIONS_STRING = ''
