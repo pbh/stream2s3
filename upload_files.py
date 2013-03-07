066772341e6fce0a650fbfb9c937a386cb7e559d
Artem Dudarev <dudarev@gmail.com>
2013-03-07 16:43:48 +0200

    17: cleaning bucket if its size is larger than maximum

--- upload_files.py
+++ upload_files.py
@@ -1,13 +1,45 @@
 import os
 
-import config
-
 from boto.s3.connection import S3Connection
 from boto.s3.key import Key
 
+import config
+
+
+# connection to bucket
+
 conn = S3Connection(
     config.AWS_ACCESS_KEY_ID, config.AWS_SECRET_ACCESS_KEY)
-bucket = conn.create_bucket('stream2s3')
+
+bucket = conn.create_bucket(config.BUCKET_NAME)
+
+
+# check bucket size and clean if necessary
+
+total_bucket_size = 0
+for key in bucket:
+    total_bucket_size += key.size
+
+Mb = 1024 ** 2
+print 'Total bucket size (Mb): {0:.2f}'.format(total_bucket_size / float(Mb))
+print 'Maximum size specified in congif.py (Mb): {0:.2f}'.format(
+    config.MAX_BUCKET_SIZE_BYTES / float(Mb))
+
+if total_bucket_size > config.MAX_BUCKET_SIZE_BYTES:
+    print 'Cleaning bucket (oldest files first)'
+    new_total_bucket_size = total_bucket_size
+    count_deleted = 0
+    for key in sorted(bucket, key=lambda x: x.last_modified):
+        new_total_bucket_size -= key.size
+        bucket.delete_key(key)
+        print 'deleted: {}'.format(key.name)
+        count_deleted += 1
+        if new_total_bucket_size < config.MAX_BUCKET_SIZE_BYTES:
+            print 'deleted keys: {}'.format(count_deleted)
+            break
+
+
+# upload files
 
 for dirpath, dirnames, filenames in os.walk(config.DESTINATION_DIRECTORY):
     print 'dirpath=', dirpath
