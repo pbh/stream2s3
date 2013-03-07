dc297d4048d5f0b389f4085671fb3a436eba74d5
Artem Dudarev <dudarev@gmail.com>
2013-03-07 16:49:12 +0200

    26: cleaned up and refactored

--- stream2s3.py
+++ stream2s3.py
@@ -1,15 +1,20 @@
-import config
 import shlex
 from subprocess import call
 
+import config
+
+
 options_str = config.STREAM_URL + ' ' + config.STREAMRIPPER_OPTIONS_STRING
 
+# destination directory
 if not '-d' in options_str and config.DESTINATION_DIRECTORY:
     options_str += ' -d {0}'.format(config.DESTINATION_DIRECTORY)
 
+# number of seconds to run
 if not '-l' in options_str and config.NUMBER_OF_SECONDS_TO_RUN:
     options_str += ' -l {0}'.format(config.NUMBER_OF_SECONDS_TO_RUN)
 
 options_list = shlex.split(options_str)
-print options_list
+
+
 call(['streamripper'] + options_list)
