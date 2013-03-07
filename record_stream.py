72edfd00235f33c73765439da13c15a76929665b
Artem Dudarev <dudarev@gmail.com>
2013-03-07 20:42:19 +0200

    more comments and logs

--- record_stream.py
+++ record_stream.py
@@ -1,8 +1,11 @@
 import shlex
 from subprocess import call
+import datetime
 
 import config
 
+
+# creating list of options
 
 options_str = config.STREAM_URL + ' ' + config.STREAMRIPPER_OPTIONS_STRING
 
@@ -17,4 +20,11 @@ if not '-l' in options_str and config.NUMBER_OF_SECONDS_TO_RUN:
 options_list = shlex.split(options_str)
 
 
+# running streamripper
+
+print 'starting recording: {}'.format(datetime.datetime.now())
+print 'options used to run: {}'.format(options_str)
+
 call(['streamripper'] + options_list)
+
+print 'finished recording: {}'.format(datetime.datetime.now())
